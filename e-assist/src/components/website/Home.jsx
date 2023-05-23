02bc6f004b067329064a7c6ff156798615c0ee76
AsadAhmad1453 <94599676+AsadAhmad1453@users.noreply.github.com>
2023-05-24 01:38:00 +0500

    changes

--- e-assist/src/components/website/Home.jsx
+++ e-assist/src/components/website/Home.jsx
@@ -12,6 +12,7 @@ import camera from '../../assets/images/camera.png';
 import search from '../../assets/images/search.png';
 import ranking from '../../assets/images/ranking.png';
 import user from '../../assets/images/username.png';
+import tickorange from '../../assets/images/tickorange.png'
 
 
 
@@ -46,11 +47,11 @@ function Home(){
                 </div>
                 <div className='first-content mt-3'>
                     <ul>
-                        <li className='mt-2'><img src={tick}/>Easy to use</li>
-                        <li className='mt-2'><img src={tick}/>Amazon Integration</li>
-                        <li className='mt-2'><img src={tick}/>Image Generator</li>
-                        <li className='mt-2'><img src={tick}/>Visual Listing optimization</li>
-                        <li className='mt-2'><img src={tick}/>High Quality Support</li>
+                        <li className='mt-2'><img src={tickorange}/>Easy to use</li>
+                        <li className='mt-2'><img src={tickorange}/>Amazon Integration</li>
+                        <li className='mt-2'><img src={tickorange}/>Image Generator</li>
+                        <li className='mt-2'><img src={tickorange}/>Visual Listing optimization</li>
+                        <li className='mt-2'><img src={tickorange}/>High Quality Support</li>
                     </ul>
                 </div>
             </div>
@@ -133,110 +134,164 @@ function Home(){
                 </a>         
             </div>
         </div>
+</div>  
 
-        <div className='row mt-4'>
-            <div className='col-12  services'>
-                    <h2>Our Customers</h2>
-                </div>
-            <div class="col-md-4 col-lg-4 col-sm-12 mt-4">
-                <div class="review-card p-3">
-                <div>
-                    <img src={user} width="70px" height="70px"/>
-                    <span className='ml-2'>Theresa.</span>
-                <span class="rating ml-3"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></span> 
-                </div>
-                <div class="mt-2">
-                    <span>I tuly beileive that they are giving their best to find quality product that will rank on amazon and give profitable earning </span>
-
-                </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 col-lg-4 col-sm-12 mt-4">
-                <div class="review-card p-3">
-                <div>
-                    <img src={user} width="70px" height="70px"/>
-                    <span className='ml-2'>John</span>
-                <span class="rating ml-3"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></span> 
-                </div>
-                <div class="mt-2">
-                    <span>I tuly beileive that they are giving their best to find quality product that will rank on amazon and give profitable earning </span>
-
-                </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-lg-4 col-sm-12 mt-4">
-                <div class="review-card p-3">
-                <div>
-                    <img src={user} width="70px" height="70px"/>
-                    <span className='ml-2'>Alice</span>
-                <span class="rating ml-3"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></span> 
-                </div>
-                <div class="mt-2">
-                    <span>I tuly beileive that they are giving their best to find quality product that will rank on amazon and give profitable earning </span>
-
-                </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div className='row mt-4'>
-            <div className='col-12  services'>
-                 <h2>Pricing</h2>
-            </div>
-            <div className='col-md-6 d-flex justify-content-center p-4'>
-                <div className='price-card'>
-                    <div className='heading'>
-                        <h4>Monthly</h4>
-                        <div className='price mt-3'>
-                         <h3>15$/month</h3>
+            <div className='container-fluid mt-4 '>
+                <div className='row service-banner-two mt-5 p-4'>
+                        <div className='sbtwo col-lg-6 col-md-6 col-sm-12 '>
+                            <h3 className='wcu'>Why Choose us</h3>
+                            <h1 className='wwd'>What we do?</h1>
+                            <span>Nulla ac eros vel neque mattis maximus. In et feugiat neque,<br></br> vel porta ante. Morbi at justo justo. Etiam eget eros ac enim iaculis sagittis. Maecenas eget sapien purus. Phasellus gravida viverra convallis. Ut vitae mauris tincidunt, pharetra turpis condimentum,<br></br> eleifend ipsum. Proin mollis bibendum odio, eu scelerisque lacus fermentum in.</span>
+                            <div className='row mt-4'>
+                                <div className='col-6'>
+                                    <ul>
+                                        <li><img src={tickorange}/>Customer Funnel Booster</li>
+                                        <li><img src={tickorange}/>Remarketing Blueprint</li>
+                                        <li><img src={tickorange}/>Lead Nurturing Scheme</li>
+                                    </ul>
+                                </div>
+                                <div className='col-6'>
+                                    <ul>
+                                        <li><img src={tickorange}/>Vulputate massa</li>
+                                        <li><img src={tickorange}/>Phasellus odio dolor</li>
+                                        <li><img src={tickorange}/>Aliquam sed finibus</li>
+                                    </ul>
+                                </div>  
+                            </div>
                         </div>
+                        <div className=' sbtwo col-lg-6 col-md-6 col-sm-12'>
+                            <div className='row'>
+                            <div className='col-lg-6 col-md-6 col-sm-12'>
+                                <div className='transparent-card'>
+                                    <h5>Data Driven</h5>
+                                    <span>We bring the roght people together to challenge established thinking and drive transform in 2020</span>
+                                </div>
+                                <div className='transparent-card mt-4'>
+                                    <h5>Market Analys</h5>
+                                    <span>We bring the roght people together to challenge established thinking and drive transform in 2020</span>
+                                </div>
+                            </div>
+                            <div className='col-lg-6 col-md-6 col-sm-12'>
+                                <div className='transparent-card'>
+                                    <h5>Tech Support</h5>
+                                    <span>We bring the roght people together to challenge established thinking and drive transform in 2020</span>
+                                </div>
+                                <div className='transparent-card mt-4'>
+                                    <h5>Data Secure</h5>
+                                    <span>We bring the roght people together to challenge established thinking and drive transform in 2020</span>
+                                </div>
+                            </div>
+                            </div>
+                          
+                            
+                        </div>
+                </div>
+            </div>
+
+        <div className='container'>
+            <div className='row mt-4'>
+                <div className='col-12  services'>
+                        <h2>Our Customers</h2>
                     </div>
-                    <div className='content-price mt-4'>
-                        <ul>
-                            <li className='mt-3'><img src={tick} />Up to 5 Templates</li>
-                            <li className='mt-3'><img src={tick} />1 Brand</li>
-                            <li className='mt-3'><img src={tick} />25 listing</li>
-                            <li className='mt-3'><img src={tick} />50 image background</li>
-                            <li className='mt-3'><img src={tick} />50 keyword Search</li>
-                            <li className='mt-3'><img src={tick} />3 Amazon INtegration</li>
-                            <li className='mt-3'><img src={tick} />1M Free Stock Photos</li>
-                            <li className='mt-3'><img src={tick} />250MB Storage</li>
-                        </ul>
-                        <div className='mt-3 d-flex justify-content-center'>
-                            <button>Get Started</button>
-                        </div>
+                <div class="col-md-4 col-lg-4 col-sm-12 mt-4">
+                    <div class="review-card p-3">
+                    <div>
+                        <img src={user} width="70px" height="70px"/>
+                        <span className='ml-2'>Theresa.</span>
+                    <span class="rating ml-3"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></span> 
+                    </div>
+                    <div class="mt-2">
+                        <span>I tuly beileive that they are giving their best to find quality product that will rank on amazon and give profitable earning </span>
+
+                    </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='col-md-6 d-flex justify-content-center p-4'>
-                <div className='price-card'>
-                    <div className='heading'>
-                        <h4>Yearly</h4>
-                        <div className='price mt-3'>
-                         <h3>100$/year</h3>
-                        </div>
+                <div class="col-md-4 col-lg-4 col-sm-12 mt-4">
+                    <div class="review-card p-3">
+                    <div>
+                        <img src={user} width="70px" height="70px"/>
+                        <span className='ml-2'>John</span>
+                    <span class="rating ml-3"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></span> 
                     </div>
-                    <div className='content-price mt-4'>
-                        <ul>
-                            <li className='mt-3'><img src={tick} />Up to 10 Templates</li>
-                            <li className='mt-3'><img src={tick} />3 Brand</li>
-                            <li className='mt-3'><img src={tick} />50 listing</li>
-                            <li className='mt-3'><img src={tick} />200 image background</li>
-                            <li className='mt-3'><img src={tick} />200 keyword Search</li>
-                            <li className='mt-3'><img src={tick} />6 Amazon INtegration</li>
-                            <li className='mt-3'><img src={tick} />2M Free Stock Photos</li>
-                            <li className='mt-3'><img src={tick} />1GB Storage</li>
-                        </ul>
-                        <div className='mt-3 d-flex justify-content-center'>
-                            <button>Get Started</button>
-                        </div>
+                    <div class="mt-2">
+                        <span>I tuly beileive that they are giving their best to find quality product that will rank on amazon and give profitable earning </span>
+
+                    </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4 col-sm-12 mt-4">
+                    <div class="review-card p-3">
+                    <div>
+                        <img src={user} width="70px" height="70px"/>
+                        <span className='ml-2'>Alice</span>
+                    <span class="rating ml-3"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></span> 
+                    </div>
+                    <div class="mt-2">
+                        <span>I tuly beileive that they are giving their best to find quality product that will rank on amazon and give profitable earning </span>
+
+                    </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+
+            <div className='row mt-4'>
+                <div className='col-12  services'>
+                    <h2>Pricing</h2>
+                </div>
+                <div className='col-md-6 d-flex justify-content-center p-4'>
+                    <div className='price-card'>
+                        <div className='heading'>
+                            <h4>Monthly</h4>
+                            <div className='price mt-3'>
+                            <h3>15$/month</h3>
+                            </div>
+                        </div>
+                        <div className='content-price mt-4'>
+                            <ul>
+                                <li className='mt-3'><img src={tickorange} />Up to 5 Templates</li>
+                                <li className='mt-3'><img src={tickorange} />1 Brand</li>
+                                <li className='mt-3'><img src={tickorange} />25 listing</li>
+                                <li className='mt-3'><img src={tickorange} />50 image background</li>
+                                <li className='mt-3'><img src={tickorange} />50 keyword Search</li>
+                                <li className='mt-3'><img src={tickorange} />3 Amazon INtegration</li>
+                                <li className='mt-3'><img src={tickorange} />1M Free Stock Photos</li>
+                                <li className='mt-3'><img src={tickorange} />250MB Storage</li>
+                            </ul>
+                            <div className='mt-3 d-flex justify-content-center'>
+                                <button>Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-6 d-flex justify-content-center p-4'>
+                    <div className='price-card'>
+                        <div className='heading'>
+                            <h4>Yearly</h4>
+                            <div className='price mt-3'>
+                            <h3>100$/year</h3>
+                            </div>
+                        </div>
+                        <div className='content-price mt-4'>
+                            <ul>
+                                <li className='mt-3'><img src={tickorange} />Up to 10 Templates</li>
+                                <li className='mt-3'><img src={tickorange} />3 Brand</li>
+                                <li className='mt-3'><img src={tickorange} />50 listing</li>
+                                <li className='mt-3'><img src={tickorange} />200 image background</li>
+                                <li className='mt-3'><img src={tickorange} />200 keyword Search</li>
+                                <li className='mt-3'><img src={tickorange} />6 Amazon INtegration</li>
+                                <li className='mt-3'><img src={tickorange} />2M Free Stock Photos</li>
+                                <li className='mt-3'><img src={tickorange} />1GB Storage</li>
+                            </ul>
+                            <div className='mt-3 d-flex justify-content-center'>
+                                <button>Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
        
         
        </div>
--- e-assist/src/components/website/Producthunting.jsx
+++ e-assist/src/components/website/Producthunting.jsx
@@ -7,6 +7,9 @@ import chart from '../../assets/images/chart.png';
 import report from '../../assets/images/report.png';
 import dollar from '../../assets/images/dollar.png';
 import optimization from '../../assets/images/optimization.png';
+import serviceillustration from '../../assets/images/serviceillustration.jpg';
+import servicebanner from '../../assets/images/servicebannerthree.jpg';
+
 
 
 function Producthunting(){
@@ -60,9 +63,82 @@ function Producthunting(){
                             <img src={optimization} width="70px" height="70px"/>
                             <h4 className='mt-2'>Product list optimization</h4>
                             <span>We bring the right people together to challenge established thinking and drive transform in 2020</span>
-                        </div>
-                        
+                        </div>                      
                     </div>
+                </div>
+
+                <div className='row mt-4 ourvision'>
+                    <div className='col-lg-6 col-md-6 col-sm-12 p-4'>
+                    <img src={serviceillustration} class="img-fluid" alt="Responsive image"/>
+                    </div>
+                    <div className='col-lg-6 col-md-6 col-sm-12 p-4'>
+                        <h5>Our Vision</h5>
+                        <h2>We Craft Bright Future</h2>
+                        <span>In et feugiat neque, vel porta ante. Morbi at justo justo. Etiam eget eros ac enim iaculis sagittis. Maecenas eget sapien purus. </span>
+                        <div className='row'>
+                           <div className='col-lg-6 mt-3 col-md-6 col-sm-12'>
+                                 <div className='servicetwocard'>
+                                    <h6>Managed it service</h6>
+                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                                </div>
+                            </div>
+                            <div className='col-lg-6 mt-3 col-md-6 col-sm-12'>
+                                 <div className='servicetwocard'>
+                                    <h6>Cloud Security</h6>
+                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                                </div>
+                            </div>
+                            <div className='col-lg-6 col-md-6 col-sm-12'>
+                                 <div className='servicetwocard'>
+                                    <h6>Cloud Data</h6>
+                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                                </div>
+                            </div>
+                            <div className='col-lg-6 col-md-6 col-sm-12'>
+                                 <div className='servicetwocard'>
+                                    <h6>Connectvity</h6>
+                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className='row service-banner-three mt-5 p-4'>
+                        <div className='sbtwo col-lg-6 col-md-6 col-sm-12 '>
+                            <h3 className='wcu'>Why Choose us</h3>
+                            <h1 className='wwd'>What we do?</h1>
+                            <span>Nulla ac eros vel neque mattis maximus. In et feugiat neque,<br></br> vel porta ante. Morbi at justo justo. Etiam eget eros ac enim iaculis sagittis. Maecenas eget sapien purus. Phasellus gravida viverra convallis. Ut vitae mauris tincidunt, pharetra turpis condimentum,<br></br> eleifend ipsum. Proin mollis bibendum odio, eu scelerisque lacus fermentum in.</span>
+                            <h3 className='mt-4 '>Intresting?</h3>
+                            <button className='mt-4 button-get-started'>Let's Start</button>
+                        </div>
+                        <div className=' sbtwo col-lg-6 col-md-6 col-sm-12'>
+                            <div className='row'>
+                            <div className='col-lg-6 col-md-6 col-sm-12'>
+                                <div className='transparent-card'>
+                                    <h5>Data Driven</h5>
+                                    <span>We bring the roght people together to challenge established thinking and drive transform in 2020</span>
+                                </div>
+                                <div className='transparent-card mt-4'>
+                                    <h5>Market Analys</h5>
+                                    <span>We bring the roght people together to challenge established thinking and drive transform in 2020</span>
+                                </div>
+                            </div>
+                            <div className='col-lg-6 col-md-6 col-sm-12'>
+                                <div className='transparent-card'>
+                                    <h5>Tech Support</h5>
+                                    <span>We bring the roght people together to challenge established thinking and drive transform in 2020</span>
+                                </div>
+                                <div className='transparent-card mt-4'>
+                                    <h5>Data Secure</h5>
+                                    <span>We bring the roght people together to challenge established thinking and drive transform in 2020</span>
+                                </div>
+                            </div>
+                            </div>
+                          
+                            
+                        </div>
                 </div>
             </div>
 
